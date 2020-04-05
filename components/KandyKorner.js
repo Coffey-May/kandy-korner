01713e8c064fef914b2984d94e84089d4def39d7
Coffey May <jcoffeymay6@gmail.com>
2020-04-05 17:34:38 -0500

    test

---
+++ components/KandyKorner.js
@@ -0,0 +1,27 @@
+import React from "react"
+import NavBar from "./nav/NavBar"
+import ApplicationView from "./ApplicationView"
+import "./KandyKorner.css"
+import Login from "./auth/Login"
+import { Route, Redirect } from "react-router-dom"
+import Register from "./auth/Register"
+
+export default () => (
+    <>
+        <Route render={() => {
+            if (localStorage.getItem("kandy_customer")) {
+                return (
+                    <>
+                        <Route render={props => <NavBar {...props} />} />
+                        <Route render={props => <ApplicationView {...props} />} />
+                    </>
+                )
+            } else {
+                return <Redirect to="/login" />
+            }
+        }} />
+
+        <Route path="/login" render={props => <Login {...props} />} />
+        <Route path="/register" render={props => <Register {...props} />} />
+    </>
+)
---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter as Router } from "react-router-dom"
+import KandyKorner from './components/KandyKorner';
+
+ReactDOM.render(
+    <Router>
+        <KandyKorner />
+    </Router>
+    , document.getElementById("root"))
